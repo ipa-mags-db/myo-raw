750f8ab177c291ae53422ee9d52067f33c7a69bf
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2017-05-23 11:13:59 +0200

    If message too long, just use part of message and dont crash

--- common.py
+++ common.py
@@ -4,7 +4,10 @@ def pack(fmt, *args):
     return struct.pack('<' + fmt, *args)
 
 def unpack(fmt, *args):
-    return struct.unpack('<' + fmt, *args)
+    pay, *rest = args
+    pay = pay[:struct.calcsize(fmt)]
+    return struct.unpack('<' + fmt, pay)
+
 
 def text(scr, font, txt, pos, clr=(255,255,255)):
     scr.blit(font.render(txt, True, clr), pos)
