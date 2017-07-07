58e0f83872b19771987dba69ac63b25b2e22d322
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2017-07-07 14:00:32 +0200

    make myo_test only use CPU Tensorflow in order to obtain universality

--- myo_test.py
+++ myo_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from myo_raw import MyoRaw
 import keras
 from keras.models import load_model
+import os
 
 buffer = list()
 counter = 0
@@ -10,7 +11,7 @@ def callback(emg, moving, times=[]):
     global buffer
     global counter
 
-    buffer.append(np.squeeze(m2.predict(np.array(emg,ndmin=2))))
+    buffer.append(np.squeeze(m2.predict(np.expand_dims(np.expand_dims(np.array(emg,ndmin=2),axis=1),axis=-1))))
 
     counter += 1
 
@@ -26,7 +27,10 @@ def callback(emg, moving, times=[]):
         buffer.pop(0)
 
 if __name__ == '__main__':
-    m2 = load_model("/home/myo/models/v1.h5")
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    m2 = load_model("/home/myo/models/network_1_194_178_9811.h5")
+
     print(m2.summary())
     m = MyoRaw()
 
